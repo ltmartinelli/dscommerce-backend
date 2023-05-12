9c44e7065a84853cb1c045c1bef73eaf3a67290f
ltmartinelli <lucastrindademartinelli@gmail.com>
2023-05-12 13:50:46 -0300

    Get Product by ID

---
+++ src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -0,0 +1,26 @@
+package com.devsuperior.dscommerce.services;
+
+import com.devsuperior.dscommerce.dto.ProductDTO;
+import com.devsuperior.dscommerce.entities.Product;
+import com.devsuperior.dscommerce.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Transactional(readOnly = true)
+    public ProductDTO findById(Long id){
+        Optional<Product> result = productRepository.findById(id);
+        Product product = result.get();
+        ProductDTO productDTO = new ProductDTO(product);
+        return productDTO;
+    }
+
+}
