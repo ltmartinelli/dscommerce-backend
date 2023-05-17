63c062d339f170b2487699eae7ba57eb58a32c6a
ltmartinelli <lucastrindademartinelli@gmail.com>
2023-05-17 00:32:39 -0300

    Customized Search

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -27,8 +27,10 @@ public class ProductController {
     }
 
     @GetMapping
-    public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable){
-        return ResponseEntity.ok(productService.findAll(pageable));
+    public ResponseEntity<Page<ProductDTO>> findAll(
+            @RequestParam(name = "name", defaultValue = "") String name,
+            Pageable pageable){
+        return ResponseEntity.ok(productService.findAll(name, pageable));
     }
 
     @PostMapping
